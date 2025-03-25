29240136c13f9bf5a98380bade6c799ab1087bc7
Roshaan Ali <roshaanalimeher@gmail.com>
2025-03-25 21:30:40 +0000

    created a new query from the website sql-practice.com

--- sql practie/queries.sql
+++ sql practie/queries.sql
@@ -1,2 +1,4 @@
 select province_id, sum(height) as sum_height from patients group by province_id having sum(height) >= 7000;
 select (max(weight) - min(weight)) as weight_delta from patients where last_name = 'Maroni';
+
+select (day(admission_date) as day_number, count(patient_id) as number_of_admissions group by day(admission_date)) from admissions order by number_of_admissions;
